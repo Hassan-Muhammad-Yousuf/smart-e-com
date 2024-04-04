c53563cd555714887b68e2b4fbf301d0e2b3a5a4
Hassan Muhammad Yousuf <alih3766@gmail.com>
2024-04-04 22:51:02 +0200

    Searchbar Done with misplacement

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,8 @@
 import Container from "./Container";
 import React from "react";
 import Logo from "./Logo";
+import { IoCartOutline } from "react-icons/io5";
+import { FiSearch } from "react-icons/fi";
 
 const Header = () => {
     return (
@@ -8,10 +10,11 @@ const Header = () => {
         <Container className="h-full flex items-start md:gap-x-5 justify-between md:justify-start">
             <Logo />
             {/*  Search Bar */}
-            <div>
+            <div className="w-full bg-white hidden md:flex items-center gap-x-1 border-[1px] border-lightText/50 rounded-full px-4 py-1.5 focus-within:border-orange-600">
+                <FiSearch />
                 <input type="text" 
                 placeholder="Search for Products"
-                className="placeholder:text-sm"/>
+                className="placeholder:text-sm flex-1 outline-none"/>
             </div> 
             {/*  Login */}
             {/*  Cart */}
