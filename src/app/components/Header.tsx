a9f723ed1cc7902cba5c976b995b5b7f324cbd12
Hassan Muhammad Yousuf <alih3766@gmail.com>
2024-04-05 16:39:08 +0200

    Header Complete

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,9 @@
 import Container from "./Container";
 import React from "react";
 import Logo from "./Logo";
-import { IoCartOutline } from "react-icons/io5";
+import { IoMdCart } from "react-icons/io";
 import { FiSearch } from "react-icons/fi";
+import { AiOutlineUser } from "react-icons/ai";
 
 const Header = () => {
     return (
@@ -10,14 +11,23 @@ const Header = () => {
         <Container className="h-full flex items-start md:gap-x-5 justify-between md:justify-start">
             <Logo />
             {/*  Search Bar */}
-            <div className="w-full bg-white hidden md:flex items-center gap-x-1 border-[1px] border-lightText/50 rounded-full px-4 py-1.5 focus-within:border-orange-600">
-                <FiSearch />
+            <div className="w-full bg-white hidden md:flex items-center gap-x-1 border-[1px] border-lightText/50 rounded-full px-4 py-1.5 focus-within:border-orange-600 group">
+                <FiSearch className="text-grey-500 group-focus-within::text-darkText duration-200"/>
                 <input type="text" 
                 placeholder="Search for Products"
                 className="placeholder:text-sm flex-1 outline-none"/>
             </div> 
             {/*  Login */}
+            <div className="headerDiv">
+              <AiOutlineUser className="text-2xl"/>  
+              <p className="text-sm font-semibold"> Login / Register </p>
+            </div>
             {/*  Cart */}
+            <div className="bg-black hover:bg-slate-950 rounded-full text-slate-100 hover:text-white flex items-center justify-center gap-x-1 px-3 py-1.5 border-[1px] border-black hover:border-orange-600 duration-200 relative">
+              <IoMdCart className="text-xl"/>  
+              <p className="text-sm font-semibold"> $0.00 </p>
+              <span className="bg-white text-orange-600 rounded-full text-xs font-semibold absolute -right-2 -top-1 w-5 h-5 flex items-center justify-center shadow-xl shadow-black">0</span>
+            </div>
         </Container>
     </div>
     );
