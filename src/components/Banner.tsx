106d97de85f8cf5962cf24daff901dda93d319b4
Hassan Muhammad Yousuf <alih3766@gmail.com>
2024-04-06 01:41:50 +0200

    Banner Complete

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,36 +3,69 @@ import Slider from "react-slick";
 import Banner1 from "@/images/Banner1.jpg";
 import Banner2 from "@/images/Banner2.jpg";
 import Banner3 from "@/images/Banner3.jpg";
-import Banner4 from "@/images/Banner4.jpg";
+import { PiCaretLeftLight, PiCaretRightLight } from "react-icons/pi";
+import Image from "next/image";
+import BannerText from "./BannerText";
+
 
 const Banner = () => {
+    const NextArrow=(props:any)=>{
+        const {onClick} = props;
+        return(
+        <div  className="p-3 bg-slate-100 hover:text-orange-600
+        hover:bg-white cursor-pointer duration-200 rounded-full 
+        text-2xl flex items-center justify-center z-20 absolute 
+        left-2 top-1/2"
+        onClick={onClick}>
+            <PiCaretLeftLight />
+        </div>
+        );
+    };
+    const PrevArrow=(props:any)=>{
+        const {onClick} = props;
+        return(
+        <div className="p-3 bg-slate-100 hover:text-orange-600
+        hover:bg-white cursor-pointer duration-200 rounded-full 
+        text-2xl flex items-center justify-center z-20 absolute 
+        right-2 top-1/2"
+        onClick={onClick}>
+            <PiCaretRightLight />
+        </div>
+        );
+    };
     var settings = {
-        dots: true,
+        dots: false,
         infinite: true,
-        speed: 500,
+        autoplay: true,
         slidesToShow: 1,
-        slidesToScroll: 1
-    }
+        slidesToScroll: 1,
+        arrows: true,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+    };
     return (
         <div>
         <Slider {...settings}>
-        <div>
-            <h3>1</h3>
+        <div className="w-full h-full relative">
+            <Image 
+            src = {Banner1} 
+            alt="Banner1" 
+            className="w-full h-full relative" />
+        <BannerText title = "Women's Choice"/>
         </div>
-        <div>
-            <h3>2</h3>
+        <div className="w-full h-full relative">
+            <Image 
+            src = {Banner2} 
+            alt="Banner2" 
+            className="w-full h-full relative" />
+        <BannerText title = "For Men's Wardrobe"/>
         </div>
-        <div>
-            <h3>3</h3>
-        </div>
-        <div>
-            <h3>4</h3>
-        </div>
-        <div>
-            <h3>5</h3>
-        </div>
-        <div>
-            <h3>6</h3>
+        <div className="w-full h-full relative">
+            <Image 
+            src = {Banner3} 
+            alt="Banner3" 
+            className="w-full h-full relative" />
+        <BannerText title = "Our Offers"/>
         </div>
         </Slider>
         </div>
